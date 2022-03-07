34a1a7ba69819bf8d46e43d48a8b52cbc20ac060
Mantas Vainutis <61165571+TooChEasy@users.noreply.github.com>
2022-03-07 22:00:35 +0200

    Added pagination

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="overflow-auto">
+    <b-table
+      :items="items"
+      :perPage="itemsPerPage"
+      :current-page="currentPage"
+    >
+    </b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="itemsPerPage"
+    ></b-pagination>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PaginationComponent',
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 2,
+    };
+  },
+  props: {
+    items: Array,
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    },
+  },
+};
+</script>
--- src/views/UserListView.vue
+++ src/views/UserListView.vue
@@ -1,29 +1,10 @@
 <template>
   <div class="userList">
     <div class="container">
-      <table v-show="!isUserListEmpty" class="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Age</th>
-            <th scope="col">Gender</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="user in users" :key="user.id">
-            <td>{{ user.name }}</td>
-            <td>{{ user.lastName }}</td>
-            <td>{{ user.email }}</td>
-            <td>{{ user.age }}</td>
-            <td>{{ user.gender }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <PaginationComponent v-show="!isUserListEmpty" :items="users" />
       <div>
         <h1 class="text-center" v-show="isUserListEmpty">
-          Sorry, there is no users in this category
+          Sorry, there are no users in this category
         </h1>
       </div>
     </div>
@@ -31,8 +12,13 @@
 </template>
 
 <script>
+import PaginationComponent from '@/components/PaginationComponent.vue';
+
 export default {
   name: 'UserListView',
+  components: {
+    PaginationComponent,
+  },
   data() {
     return {
       users: [],
@@ -68,6 +54,10 @@ export default {
       return this.$route.params.cat.substring(
         this.$route.params.cat.lastIndexOf('/') + 1,
       );
+    },
+    getCurrentPage(value) {
+      console.log('Pag', this.currentPage);
+      this.currentPage = value;
     },
   },
 };
